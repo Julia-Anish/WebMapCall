a29740c9bc6defa54258df51eec4d3de377531f0
Julia Anish <julia.anishchenko@gmail.com>
2019-12-23 17:20:24 +0200

    the first button opens the link in the browser, the second opens the map and finds the coordinates, the third calls the number

--- app/src/main/java/com/example/webmapcall/MainActivity.java
+++ app/src/main/java/com/example/webmapcall/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.webmapcall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnWeb, btnMap,btnCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnWeb = (Button)findViewById(R.id.btnWeb);
+        btnMap = (Button)findViewById(R.id.btnMap);
+        btnCall = (Button)findViewById(R.id.btnCall);
+
+        btnWeb.setOnClickListener(this);
+        btnMap.setOnClickListener(this);
+        btnCall.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.btnWeb:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://metanit.com/java/android/3.10.php"));
+                startActivity(intent);
+                break;
+            case R.id.btnMap:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo: -0.45609946, -90.26607513"));
+                startActivity(intent);
+                break;
+            case R.id.btnCall:
+                intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("+380999640573"));
+                startActivity(intent);
+                break;
+        }
+
     }
 }
